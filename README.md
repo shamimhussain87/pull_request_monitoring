c21c25dd1cfd4f03db69f630f859e57d3d73b343
shamimhussain87 <95405996+shamimhussain87@users.noreply.github.com>
2022-01-28 08:37:07 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# pull_request_monitoring
+# github-pull-request-monitoring
+>Github pull request monitoring using PyGithub library
+>>PyGithub is a python library by which we can access Github Rest Api and perform different operations depends upon our requiremnets .In this section we focused more on repositories access and Pull requests related data .
+
+# Depenedencies to setup before working :
+1) Install PyGithub using command line : pip install PyGithub
+2) Tested on python3.6 or above
+3) Import Datetime library for time claculation purpose of pull requests
+4) Generate token from console of Github to access or read data related to that particular account
+
+# Steps to follow after setting up dependencies
+
+1) Create user and personal access tokens in source and target GitHub. 
+2) User should have read privileges to source and target account.
+
+# Required inputs to the algorithm
+
+1) It requires username of github user for which you want to check pull request creation time
+2) It also requires no of pull requests for which you want to get information about there completion.
+3) You can provide the custom account creation date or the algo use the default account creation date
+
+# Output provided by an algorithm
+
+1) Output will be the total time taken by user to make pull requests on different repositories of an owner account.
+2) Or an exception scenario in which user does not make an pull request or user does not exists.
+# Actions performed
+
+ 1) Clone description, pull requests ,creation dates and users info from required repos.
+ 2) Get list of all repos from the github account.
+ 3) List all open pull requets for the given user.
+ 4) Neglect pull requests for the blacklist repositories .
+ 5) Count duration of pull request from account creation till the last pull request.
+ 6) Get pull requests data of user based on the pull requests done on main/master branch
+ 
+# Source: 
+Simple example of the usage of the PyGithub python module
+
+https://pygithub.readthedocs.io/en/latest/introduction.html
+
---
+++ main.py
@@ -0,0 +1,96 @@
+import datetime
+from github import Github
+import sys
+
+username = sys.argv[1]
+total_prs = sys.argv[2]
+try:
+      total_prs = int(total_prs)
+except:
+      print("total prs must be integer")
+      exit()
+
+date = None
+if len(sys.argv)==4:
+      date = sys.argv[3]
+
+if username=='':
+      print("username cannot be null")
+      sys.exit(1)
+
+user_reg_date = ''
+if date!=None:
+      try:
+            year, month, day = map(int, date.split('-'))
+            user_reg_date = datetime.date(year, month, day)
+            user_reg_date = datetime.datetime.strptime(user_reg_date.__str__(), '%Y-%m-%d')
+      except:
+            print("not a valid date")
+            sys.exit(1)
+
+# First create a Github instance:
+# using an access token
+g = Github("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")
+try:
+      g.get_user(username) == True
+      # print("user exists")
+except:
+
+      print("user does not exits.Please try again....")
+      exit()
+
+count = 0
+f = open("demofile.txt", "r")
+line = f.read()
+if user_reg_date=='':
+      try:
+            user_reg_date = g.get_user(username).created_at
+      except:
+            print("user doesnot exists or entered wrong username .Please tr again ...")
+            sys.exit(1)
+
+# print("Loading ..........................")
+def pulls(repo, requests, maxD,
+          user_reg_date):  # function parameters refer to global variables which compute time and requests of all repos combined
+      # Github Enterprise with custom hostname
+      pulls = repo.get_pulls(state="all", sort='created', base='main')  # get all pull requests
+
+      totalRequests = 0  # function param => calculating time difference of this repo only
+      maxDate = ""  # function param
+      for pr in pulls:
+            if pr.created_at>=user_reg_date:
+                  if (repo.get_pull(pr.number).user.login == username):
+                        totalRequests += 1
+                        requests += 1
+                        maxDate = repo.get_pull(pr.number).created_at
+                  if requests >= int(total_prs):
+                        break
+
+      if maxDate == "":  # check if no pulls are made by that specific user or no pulls exists in this specific repo at all
+            # print("No pull request in " + repo.name);
+            return requests, maxD  # returning tuple
+
+      if maxD != None:  # if function maxDate is greater or equal to the global maxDate -> replace it with new date
+            if maxDate >= maxD:
+                  maxD = maxDate
+      else:
+            maxD = maxDate
+      return requests, maxD
+
+
+maxDate = None
+totalRequests = 0
+for repo in g.get_user().get_repos(sort="ascending"):  # get all repositories
+      if line.__contains__(repo.name):
+            # print("skipping repo "+repo.name)
+            continue
+
+      totalRequests, maxDate = pulls(repo, totalRequests, maxDate,
+                                     user_reg_date)  # Get all pull requests based on the repository names and receiving tuples to populate total requests(of all repos) and dates(min,max of all repos)
+      if totalRequests >= int(total_prs):
+            break
+
+if (totalRequests < int(total_prs)):
+      print("User has not made any pull request yet or the pull requests are less than " + total_prs.__str__())
+elif (maxDate != None):
+      print(username + " made " + totalRequests.__str__() + " pull requests in ", (maxDate - user_reg_date).days, " days")
